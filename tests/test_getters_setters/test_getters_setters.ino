b98b37f07aa49f1b91a1599d520daf3ec122a6e8
MiguelLoureiro98 <miguel.santos.loureiro@gmail.com>
2024-12-16 17:56:56 +0000

    Tested AR getters (passing).

--- tests/test_getters_setters/test_getters_setters.ino
+++ tests/test_getters_setters/test_getters_setters.ino
@@ -1,9 +1,10 @@
-#include "AUnit.h"
+#line 2 "test_getters_setters.ino"
+#include <ArduinoUnit.h>
 #include "TimeSeries.h"
 
-int i = 0;
+//int i = 0;
 
-/*void testAR(){
+test(AR_getters){
 
   double w[2] = {1.2, 7.6};
   double cons = 3.5;
@@ -12,21 +13,23 @@ int i = 0;
   double *weights = model.get_weights();
   double constant = model.get_constant();
 
-  assertTrue(1);
-  //assertNear(constant, cons, 0.001);
+  assertNear(constant, cons, 0.00001);
+  assertNear(weights[0], w[0], 0.00001);
+  assertNear(weights[1], w[1], 0.00001);
 
-}*/
+}
 
 void setup() {
 
   Serial.begin(9600);
+  while(!Serial) {}
   //aunit::TestRunner::setVerbosity(aunit::Verbosity::kAll);
 
 }
 
 void loop() {
 
-  if(i == 0){
+  /*if(i == 0){
 
     double w[2] = {1.2, 7.6};
     double cons = 3.5;
@@ -41,7 +44,8 @@ void loop() {
 
     i++;
 
-  }
+  }*/
   //aunit::TestRunner::run();
+  Test::run();
 
 }
