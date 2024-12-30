24c5798f61bb2c9d5894934aee3825ce3dc648b2
MiguelLoureiro98 <miguel.santos.loureiro@gmail.com>
2024-12-30 11:28:34 +0000

    Started writing tests for forecasting methods. Deleted superfluous test files.

---
+++ tests/test_forecast/test_forecast.ino
@@ -0,0 +1,27 @@
+#include <ArduinoUnit.h>
+#include "TimeSeries.h"
+
+test(AR_forecast){
+
+
+
+}
+
+test(MA_forecast){
+
+  
+
+}
+
+void setup() {
+
+  Serial.begin(9600);
+  while(!Serial) {}
+
+}
+
+void loop() {
+
+  Test::run();
+
+}
